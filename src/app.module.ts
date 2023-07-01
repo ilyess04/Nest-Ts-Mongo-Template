d85e06f21cc3e3ce560b36ab67b11b7898f5db98
Ilyess Sellami <ilyessellami04@gmail.com>
2023-07-01 22:43:51 +0200

    swagger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './common/email/email.module';
 import { PdfModule } from './common/pdf/pdf.module';
+import { DatabaseModule } from './common/mongoose/database/database.module';
 
 @Module({
-  imports: [EmailModule, PdfModule],
+  imports: [EmailModule, PdfModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
