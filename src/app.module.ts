9c620682a05e9bcb359d39aea0d7159b378e8dcb
Ilyess Sellami <ilyessellami04@gmail.com>
2023-07-01 18:27:46 +0200

    Nest pdf generator config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './common/email/email.module';
+import { PdfModule } from './common/pdf/pdf.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, PdfModule],
   controllers: [AppController],
   providers: [AppService],
 })
