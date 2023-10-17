ef75cceb2fba0997f5d22eee0b0afc074d3f8831
Sellami Ilyess <dev@ilyessellami.com>
2023-10-17 23:59:14 +0200

    Init project template

--- src/common/interfaces/common/index.ts
+++ src/common/interfaces/common/index.ts
@@ -1,1 +1,8 @@
-export * from './pdf';
+import { PaperFormat } from 'puppeteer';
+
+interface IGeneratePdf {
+  context: Record<string, any>;
+  templateHbs: string;
+  format?: PaperFormat;
+}
+export type { IGeneratePdf };
--- src/common/interfaces/common/pdf/index.ts
+++
@@ -1,8 +0,0 @@
-import { PaperFormat } from 'puppeteer';
-
-interface IGeneratePdf {
-  context: Record<string, any>;
-  templateHbs: string;
-  format?: PaperFormat;
-}
-export type { IGeneratePdf };
