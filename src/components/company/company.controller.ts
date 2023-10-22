2cf25286fd3cdf36e7227c18d784905e20c50063
Sellami Ilyess <dev@ilyessellami.com>
2023-10-22 20:07:14 +0200

    Create company api

--- src/components/company/company.controller.ts
+++ src/components/company/company.controller.ts
@@ -1,9 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  InternalServerErrorException,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CompanyService } from './company.service';
+import { Response } from 'express';
+import { CreateCompanyDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtStrategy } from 'src/common/strategy/jwt.srategy';
 
 @ApiTags('company')
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(AuthGuard('jwt'), JwtStrategy)
+  @Post('create')
+  async CreateCompany(@Res() res: Response, @Body() body: CreateCompanyDto) {
+    try {
+      const company = await this.companyService.CreateCompany(body);
+      return res.status(HttpStatus.OK).send({
+        statusCode: HttpStatus.OK,
+        company,
+      });
+    } catch (err) {
+      throw new InternalServerErrorException({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: err,
+      });
+    }
+  }
 }
--- src/components/company/company.service.ts
+++ src/components/company/company.service.ts
@@ -1,11 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
+import { ICreateCompany } from 'src/common/interfaces/company';
 import { Company } from 'src/common/mongoose/models/company.model';
 import { COMPANY_PROVIDER } from 'src/config';
 
 @Injectable()
 export class CompanyService {
   constructor(
-    @Inject(COMPANY_PROVIDER) private readonly userModel: Model<Company>,
+    @Inject(COMPANY_PROVIDER) private readonly companyModel: Model<Company>,
   ) {}
+  async CreateCompany(payload: ICreateCompany): Promise<Company> {
+    return await this.companyModel.create(payload);
+  }
 }
