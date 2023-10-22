dd0e38dc768a079098cd6e14bccf98311b8754ee
Sellami Ilyess <dev@ilyessellami.com>
2023-10-23 01:36:15 +0200

    Runing app

--- src/components/company/company.module.ts
+++ src/components/company/company.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyProvider } from './company.provider';
 import { CompanyController } from './company.controller';
+import { DatabaseModule } from 'src/common/mongoose/database/database.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from '../user/auth/auth.service';
+import { UserProvider } from '../user/user.provider';
 
 @Module({
-  imports: [],
-  providers: [CompanyService, ...CompanyProvider],
+  imports: [
+    DatabaseModule,
+    PassportModule.register({ defaultStrategy: ['jwt', 'refresh'] }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+    }),
+  ],
+  providers: [CompanyService, ...CompanyProvider, AuthService, ...UserProvider],
   controllers: [CompanyController],
   exports: [CompanyService, ...CompanyProvider],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 8000;
 
   const config = new DocumentBuilder()
     .setTitle('App Template')
@@ -11,8 +12,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/documentations', app, document);
 
-  await app.listen(3000);
+  await app.listen(port, () => console.log('App is runing on port :', port));
 }
 bootstrap();
