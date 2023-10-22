3775c6001393b2606988fd2e788b017b82402989
Sellami Ilyess <dev@ilyessellami.com>
2023-10-22 18:03:25 +0200

    Init user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './common/email/email.module';
 import { DatabaseModule } from './common/mongoose/database/database.module';
+import { UserModule } from './components/user/user.module';
 
 @Module({
-  imports: [EmailModule, DatabaseModule],
+  imports: [EmailModule, DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
